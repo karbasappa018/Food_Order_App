c25954ddb5787c6c6e3411b971e0208e4d89f027
karbasappa018 <karbasappatadakale@gmail.com>
2026-08-02 13:40:12 +0530

    Add top rated restaurant filter using useState

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,23 +1,34 @@
 import RestorantCard from "./RestaurantCard"; 
 import resList from "../utils/mockData";
+import { useState } from "react";
+
 
 
 
 
 const Body = () =>
 {
+    const [listOfResaurants, setListOfRestaurants] = useState(resList);
+    
     return (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={()=> {
-                console.log("Button Clicked");
-                }}>Top Rated Restorants</button>
+                const filteredList = listOfResaurants.filter(
+                    (res) => res.data.avgRating > 4
+                );
+
+                setListOfRestaurants(filteredList);
+                }}
+                >Top Rated Restorants
+                </button>
+
             </div>
             <div className="res-container">
                 {/*<RestorantCard resData={resList[0]}/>*/}
 
 
-                {resList.map((restaurant) => (
+                {listOfResaurants.map((restaurant) => (
                     <RestorantCard key={restaurant.data.id} resData={restaurant} />
                 ))}
                 
